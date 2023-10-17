5a82b5f40af945f0b7590fd13ce95ad8f97b4be4
IswaryaBandaru <148181614+IswaryaBandaru@users.noreply.github.com>
2023-10-17 14:41:43 +0530

    Delete JavaScript directory

--- JavaScript/revesed_words_in_sentence.js
+++
@@ -1,8 +0,0 @@
-function reverseWords(str) {
-    let reverseWordArr = str.split(" ").map(word => word.split("").reverse().join(""));
-    return reverseWordArr.join(" ");
-  }
-
-const prompt = require('prompt-sync')();
-let sentence = prompt("Enter sentence: ");
-console.log(reverseWords(sentence));
