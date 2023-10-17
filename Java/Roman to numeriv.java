4b7400dad89582529c94841d2d47f5e85d604130
IswaryaBandaru <148181614+IswaryaBandaru@users.noreply.github.com>
2023-10-17 13:29:11 +0530

    Add files via upload

---
+++ Java/Roman to numeriv.java
@@ -0,0 +1,72 @@
+// Program to convert Roman Numerals to Numbers
+import java.util.*;
+ 
+class RomanToNumber {
+    // This function returns value of a Roman symbol
+    int value(char r)
+    {
+        if (r == 'I')
+            return 1;
+        if (r == 'V')
+            return 5;
+        if (r == 'X')
+            return 10;
+        if (r == 'L')
+            return 50;
+        if (r == 'C')
+            return 100;
+        if (r == 'D')
+            return 500;
+        if (r == 'M')
+            return 1000;
+        return -1;
+    }
+ 
+    // Finds decimal value of a given roman numeral
+    int romanToDecimal(String romanNumeric)
+    {
+        // Initialize result
+        int res = 0;
+ 
+        for (int i = 0; i < romanNumeric.length(); i++) {
+            // Getting value of symbol s[i]
+            int s1 = value(romanNumeric.charAt(i));
+ 
+            // Getting value of symbol s[i+1]
+            if (i + 1 < romanNumeric.length()) {
+                int s2 = value(romanNumeric.charAt(i + 1));
+ 
+                // Comparing both values
+                if (s1 >= s2) {
+                    // Value of current symbol is greater or equalto the next symbol
+                    res = res + s1;
+                }
+                else {
+                    // Value of current symbol is less than the next symbol
+                    res = res + s2 - s1;
+                    i++;
+                }
+            }
+            else {
+                res = res + s1;
+            }
+        }
+ 
+        return res;
+    }
+ 
+    // Main Code
+    public static void main(String args[])
+    {
+        // Taking input from the user
+        Scanner input = new Scanner(System.in);
+        RomanToNumber ob = new RomanToNumber();
+    
+        // Considering inputs given are valid 
+        String romanNumber = input.next();
+        System.out.println("Integer form of Roman Numeral"
+                           + " is "
+                           + ob.romanToDecimal(romanNumber));
+        input.close();
+    }
+}
